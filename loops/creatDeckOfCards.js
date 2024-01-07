9e0df2930f84b673ba19d17fffb977767f894a21
Moe Ezz <121265874+MoeEzzeldin@users.noreply.github.com>
2024-01-07 03:33:13 -0500

    Update creatDeckOfCards.js

--- loops/creatDeckOfCards.js
+++ loops/creatDeckOfCards.js
@@ -15,19 +15,3 @@ for (let i = 0; i < suits.length; i++) {
 
 console.log(result);
 
-// for (i = 0 ; i < suits.length ; i++)
-// {
-//     let inner = []
-//     for (j = 0 ; j < ranks.length ; j++)
-//     {
-//         let obj = {}
-//         obj.suits = suits[i]
-//         obj.ranks = ranks[j]
-//         inner.push(obj)
-//     }
-//     result.push(inner)
-// }
-
-// console.log(result)
-// let x = result[2][0];
-// console.log(x)
